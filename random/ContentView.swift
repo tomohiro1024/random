4539359db84751f9c7b600a3f7b04c9083098efb
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-07 16:57:48 +0900

    ランダムアプリ一旦完成

--- random/ContentView.swift
+++ random/ContentView.swift
@@ -8,17 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNum = 1
+    @State private var timer: Timer?
+    @State private var teamName = "アーセナル"
+    
     var body: some View {
         VStack {
-            Image("1")
+            Spacer()
+            Image("\(randomNum)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width/2)
+                .padding()
+            Spacer()
+            Text("\(teamName)")
+                .fontWeight(.bold)
+                .font(.system(size: 30))
+            Spacer()
             Button {
-                print("tap")
+                random()
+                
             } label: {
                 Text("ランダム")
+                    .padding()
+                    .background(Color.purple)
+                    .foregroundColor(.black)
+                    .cornerRadius(20)
             }
 
         }
         .padding()
+    }
+    
+    private func random() {
+        print("tap")
+        randomNum = Int.random(in: 1...6)
+        print(randomNum)
+        switch randomNum {
+        case 1:
+            teamName = "アーセナル"
+        case 2:
+            teamName = "トッテナム"
+        case 3:
+            teamName = "マンチェスターC"
+        case 4:
+            teamName = "マンチェスターU"
+        case 5:
+            teamName = "チェルシー"
+        case 6:
+            teamName = "リヴァプール"
+        default:
+            break
+        }
+        
     }
 }
 
